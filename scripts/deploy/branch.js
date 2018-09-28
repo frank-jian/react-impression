fd01bf0e2c890f6a87752d7ea3e115df15226e5a
biped <poticr@gmail.com>
2018-09-28 13:25:58 +0800

    test upload

--- scripts/deploy/branch.js
+++ scripts/deploy/branch.js
@@ -1,2 +1,67 @@
 console.log('test deploy on branch')
-console.log(process.env)
+
+/**
+ * Config
+ */
+const qiniu = require('qiniu')
+
+const config = new qiniu.conf.Config()
+config.zone = qiniu.zone.Zone_na0
+config.useHttpsDomain = true
+config.useCdnDomain = true
+
+const { ACCESS_KEY, SECRET_KEY, BUCKET } = process.env
+const mac = new qiniu.auth.digest.Mac(ACCESS_KEY, SECRET_KEY)
+const putPolicy = new qiniu.rs.PutPolicy({ scope: BUCKET })
+const uploadToken = putPolicy.uploadToken(mac)
+
+/**
+ * Files
+ */
+const path = require('path')
+const glob = require('glob')
+
+const BASE = 'packages/react-impression-website/dist'
+const files = glob(`${BASE}/**/*.*`, (err, files) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+
+  return files.map(file => ({
+    localFile: file,
+    key: `react-impression/${path.relative(BASE, file)}`,
+  }))
+})
+
+/**
+ * Upload
+ */
+const formUploader = new qiniu.form_up.FormUploader(config)
+const putExtra = new qiniu.form_up.PutExtra()
+
+const callback = (err, body, info) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+
+  if (info.statusCode === '200') {
+    console.log('upload successed!')
+    console.log(body)
+  } else {
+    console.log('upload failed!')
+    console.log(respInfo.statusCode)
+    console.log(respBody)
+  }
+}
+
+files.forEach(file =>
+  formUploader.putFile(
+    uploadToken,
+    file.key,
+    file.localFile,
+    putExtra,
+    callback
+  )
+)
