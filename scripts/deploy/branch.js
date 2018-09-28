db8b1caca56faa4f0dceba6a9419582f9015b29f
biped <poticr@gmail.com>
2018-09-28 13:46:33 +0800

    Test upload

--- scripts/deploy/branch.js
+++ scripts/deploy/branch.js
@@ -1,12 +1,9 @@
-console.log('test deploy on branch')
-
 /**
  * Config
  */
 const qiniu = require('qiniu')
 
 const config = new qiniu.conf.Config()
-config.zone = qiniu.zone.Zone_na0
 config.useHttpsDomain = true
 config.useCdnDomain = true
 
@@ -16,52 +13,50 @@ const putPolicy = new qiniu.rs.PutPolicy({ scope: BUCKET })
 const uploadToken = putPolicy.uploadToken(mac)
 
 /**
+ * Upload
+ */
+const formUploader = new qiniu.form_up.FormUploader(config)
+const putExtra = new qiniu.form_up.PutExtra()
+
+const upload = file =>
+  formUploader.putFile(
+    uploadToken,
+    file.key,
+    file.localFile,
+    putExtra,
+    (err, body, info) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+
+      if (info.statusCode === 200) {
+        console.log('Upload success: ', file.key)
+      } else {
+        console.log('Upload failure: ', `${info.statusCode} ${body.error}`)
+        console.log('                ', file.key)
+      }
+    }
+  )
+
+/**
  * Files
  */
 const path = require('path')
 const glob = require('glob')
 
 const BASE = 'packages/react-impression-website/dist'
-const files = glob(`${BASE}/**/*.*`, (err, files) => {
+
+glob(`${BASE}/**/*.*`, (err, files) => {
   if (err) {
     console.error(err)
     return
   }
 
-  return files.map(file => ({
+  const transform = file => ({
     localFile: file,
     key: `react-impression/${path.relative(BASE, file)}`,
-  }))
+  })
+
+  files.map(transform).forEach(upload)
 })
-
-/**
- * Upload
- */
-const formUploader = new qiniu.form_up.FormUploader(config)
-const putExtra = new qiniu.form_up.PutExtra()
-
-const callback = (err, body, info) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-
-  if (info.statusCode === '200') {
-    console.log('upload successed!')
-    console.log(body)
-  } else {
-    console.log('upload failed!')
-    console.log(respInfo.statusCode)
-    console.log(respBody)
-  }
-}
-
-files.forEach(file =>
-  formUploader.putFile(
-    uploadToken,
-    file.key,
-    file.localFile,
-    putExtra,
-    callback
-  )
-)
